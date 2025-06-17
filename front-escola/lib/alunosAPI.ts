5b2dae7069121c9fd3d9fc6a9ace0d8ec7196f85
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-06-17 20:45:09 -0300

    end points api no front

---
+++ front-escola/lib/alunosAPI.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { Aluno } from '@/types/Aluno';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3005/alunos'
+});
+
+export const getAlunos = () => api.get<Aluno[]>('/')
+export const getAluno = (id: number) => api.get<Aluno>(`/${id}`);
+export const createAluno = (data : Aluno) => api.post('/', data);
+export const updateAluno = (id: number, data: Partial<Aluno>) => api.patch(`/${id}`, data);
+export const deleteAluno = (id: number) => api.delete(`/${id}`);
